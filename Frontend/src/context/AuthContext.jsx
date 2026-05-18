1e25dfbeff5f5f585cbf82748e63b9192aa3c63c
okwuosatobenna-boop <okwuosatobenna@gmail.com>
2026-05-19 00:13:57 +0100

    Update AuthContext.jsx

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -1,1 +1,48 @@
 
+* {
+   background-color: white;
+   color: black;
+}
+
+
+.generate-ai-answer-container {
+   margin-top: 100px;
+   margin-left: 20px;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+}
+
+
+p, strong, label {
+   background-color: transparent !important;
+   color: black;
+}
+
+
+.generate-ai-answer-container h1 {
+   font-family: Montserrat;
+   font-size: 30px;
+   line-height: 36px;
+   font-weight: 700;
+   color: #1A1A1AFF;
+}
+
+
+.generate-ai-answer-container h2 {
+   width: 343px;
+   font-family: Open Sans;
+   font-size: 18px;
+   line-height: 28px;
+   font-weight: 400;
+   color: #5F5F5FFF;
+   margin-top: 27px;
+}
+
+
+.generate-ai-answer-container .resource-details {
+   width: 343px;
+   background: #FFFFFFFF;
+   border-radius: 8px;
+   box-shadow: 0px 0px 1px #171a1f14, 0px 0px 2px #171a1f14;
